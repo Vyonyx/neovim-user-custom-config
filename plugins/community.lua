1e4f3eda0d590fef1dbd68c456964268ab8a6b8e
personal <shaneel_kumar@live.com>
2023-05-17 14:10:31 +1200

    add previous community plugins and keymappings from old configuration

--- plugins/community.lua
+++ plugins/community.lua
@@ -2,7 +2,18 @@ return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
   -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  { import = "astrocommunity.colorscheme.kanagawa" },
+  { import = "astrocommunity.colorscheme.rose-pine" },
   { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox" }, -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+
+  { import = "astrocommunity.indent.indent-blankline-nvim" },
+  { import = "astrocommunity.indent.mini-indentscope" },
+  { import = "astrocommunity.motion.mini-bracketed" },
+  { import = "astrocommunity.motion.mini-surround" },
+  { import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.syntax.hlargs-nvim" },
+  { import = "astrocommunity.utility.neodim" },
+  { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.note-taking.neorg" },
 }
