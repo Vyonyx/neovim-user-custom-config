3d7bdea46513ac6c58d66fee81ac095c782a9251
personal <shaneel_kumar@live.com>
2023-05-17 14:11:46 +1200

    add scroll animation and enhanced symbols outline plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,32 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "declancm/cinnamon.nvim",
+    event = "VeryLazy",
+    opts = {}
+  },
+  {
+    "simrat39/symbols-outline.nvim",
+    cmd = "SymbolsOutline",
+    keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+    config = true,
+    opts = {
+      highlight_hovered_item = true,
+      show_guides = true,
+      auto_preview = false,
+      position = 'right',
+      relative_width = true,
+      width = 25,
+      auto_close = false,
+      show_numbers = false,
+      show_relative_numbers = false,
+      show_symbol_details = true,
+      preview_bg_highlight = 'Pmenu',
+      autofold_depth = nil,
+      auto_unfold_hover = true,
+      fold_markers = { '', '' },
+      wrap = false,
+    }
+  }
 }
